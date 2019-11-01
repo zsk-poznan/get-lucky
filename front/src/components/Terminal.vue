d43efa9deb9107df58b9077fd67ca792379eca2f
Arian Orwat <42947202+Arianos11@users.noreply.github.com>
2019-11-01 23:13:20 +0100

    Vue Front (#5)
    * Init
    
    * Axios connected
    
    * First End
    
    * Second End
    
    * Third End
    
    * Organization
    
    * netlify path update
    
    * Update front/src/App.vue
    
    Co-Authored-By: Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
    
    * Next End
    
    * netlify again
    
    * Netlify Again v2
    
    * Netlify v3
    
    * Netlify plz
    
    * Final
    
    * Favicon remove
    
    * Language update
    
    * Loading
    
    * update
    
    * error repair
    
    * Terminal Component
    
    * Props repair
    
    * Slot add

---
+++ front/src/components/Terminal.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="terminal">
+        <div class="code">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Terminal',
+}
+</script>
+
+<style lang="scss">
+.terminal {
+    background-color: #03303F;
+    width: 40%;
+    border-radius: 0.5rem;
+    color: white;
+    padding: 1rem 1.5rem;
+    text-align: left;
+    white-space: nowrap;
+    overflow-x: auto;
+    .code {
+      h4 {
+      display: inline;
+      font-weight: 300;
+      }
+    }
+  }
+</style>
