ebae0430389b539bdf9f9b6c73583358e3e36d98
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-11-06 10:13:17 +0100

    Fix date parsing

--- front/functions/utils.js
+++ front/functions/utils.js
@@ -1,9 +1,9 @@
 function logDate() {
-  const now = Date.now();
+  const now = new Date();
 
   const day = now.getDate();
   const month = now.getMonth();
-  const year = now.getYear();
+  const year = now.getFullYear();
 
   console.log(`${year}-${month}-${day}`);
 }
